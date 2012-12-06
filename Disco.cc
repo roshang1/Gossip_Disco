2511336c0fb32aa81285f91dfb127eed935cb805
roshang1 <roshang1@umbc.edu>
2012-12-05 19:04:21 -0500

    Working neighbor discovery.

--- Disco.cc
+++ Disco.cc
@@ -2,49 +2,83 @@
 
 Define_Module(Disco);
 
-void Disco::startup() {
-	packetsSent = 0;
-	string temp;
+double Disco::unifRandom()
+{
+	double random = rand() / double(RAND_MAX);/*
+	while(random == 0)
+		random = rand() / double(RAND_MAX);*/
+    return random;
+}
 
-	temp = "100ms";
-	setTimer(1, STR_SIMTIME(temp.c_str()));
+void Disco::startup() {
+	stringstream out;
+	string temp;
+	int startupDelay = (int) (unifRandom() * 100);
+	int index = (int) (unifRandom() * totalPrimes);
+	index = ( index == totalPrimes ) ? totalPrimes - 1 : index;
+
+	slotDuration = STR_SIMTIME(par("slotDuration"));
+	packetsSent = 0;
+	primePair[0] = primePairs[index][0];
+	primePair[1] = primePairs[index][1];
+	counter = 0;
+
+	out << startupDelay;
+	temp = out.str(); temp += "ms";
+	setTimer(START_OF_SLOT, STR_SIMTIME(temp.c_str()));
 }
 
 void Disco::timerFiredCallback(int type) {
-	string temp;
 	int i = 0;
-	int sum = 0;
 
 	switch (type) {
-	case 1:
-		trace() << "Up";
-		toNetworkLayer(createRadioCommand(SET_STATE, RX));
-		temp = "10ms";
-		setTimer(2, STR_SIMTIME(temp.c_str()));
-		for(i = 0; i < 2; i++) {
-			ExchngInfo *send = new ExchngInfo();
+	case START_OF_SLOT:
+		//trace() << "Slot starts. Slot No = " << counter;
+		counter++;
+		setTimer(START_OF_SLOT, slotDuration);
 
-			send->H = 1;
-			send->xi = 2;
-			send->wi = 3;
-			send->targetX = 3.0;
-			send->targetY = 4.0;
-			toNetworkLayer(createDiscoDataPacket(2.0, *send, packetsSent++), BROADCAST_NETWORK_ADDRESS);
+		if(counter % primePair[0] == 0 || counter % primePair[1] == 0) {
+			//Wake up and transmit
+			trace() << "Up." << counter;
+			toNetworkLayer(createRadioCommand(SET_STATE, TX));
+
+			//Send node's schedule
+			Schedule mySchedule;//= new Schedule();
+			mySchedule.primePair[0] = primePair[0];
+			mySchedule.primePair[1] = primePair[1];
+			mySchedule.currentSlotNo = counter;
+			mySchedule.currentTime = getClock();
+
+			trace() << "Start beacon.";
+			toNetworkLayer(createNeighborDiscPacket(NEIGHBOR_DISC_PACKET, mySchedule, true, packetsSent++), BROADCAST_NETWORK_ADDRESS);
+			isAsleep = false;
+		} else if (!isAsleep) {
+			//Sleepn
+			trace() << "Down." << counter;
+			isAsleep = true;
+			toNetworkLayer(createRadioCommand(SET_STATE, SLEEP));
 		}
 		break;
-	case 2:
-		trace() << "Down";
+	case GO_TO_SLEEP:
 		toNetworkLayer(createRadioCommand(SET_STATE, SLEEP));
-		temp = "990ms";
-		setTimer(1, STR_SIMTIME(temp.c_str()));
+		setTimer(START_OF_SLOT, slotDuration);
 		break;
 	}
-
 }
 
-DiscoPacket* Disco::createDiscoDataPacket(double data, ExchngInfo& extra,	unsigned int seqNum) {
-	DiscoPacket *newPacket = new DiscoPacket("Disco Msg", APPLICATION_PACKET);
-	newPacket->setData(data);
+NeighborDiscPacket* Disco::createNeighborDiscPacket(PACKET_TYPE type, Schedule& schedule, bool request, unsigned int seqNum) {
+	NeighborDiscPacket *newPacket = new NeighborDiscPacket("Neighbor discovery packet", APPLICATION_PACKET);
+	newPacket->setData(type);
+	newPacket->setSequenceNumber(seqNum);
+	newPacket->setNodeSchedule(schedule);
+	newPacket->setRequest(request);
+	newPacket->setByteLength(sizeof(int) * 2 + sizeof(long) + sizeof(simtime_t) + sizeof(bool)); // size of extradata.
+	return newPacket;
+}
+
+DataPacket* Disco::createDataPacket(PACKET_TYPE type, GossipData& extra,	unsigned int seqNum) {
+	DataPacket *newPacket = new DataPacket("Data packet", APPLICATION_PACKET);
+	newPacket->setData(type);
 	newPacket->setSequenceNumber(seqNum);
 	newPacket->setExtraData(extra);
 	newPacket->setByteLength(36); // size of extradata.
@@ -52,11 +86,52 @@ DiscoPacket* Disco::createDiscoDataPacket(double data, ExchngInfo& extra,	unsign
 }
 
 void Disco::fromNetworkLayer(ApplicationPacket * genericPacket, const char *source, double rssi, double lqi) {
-	//trace() << "Received Packet! Neighbor Discovered!";
+	int msgType = (int)genericPacket->getData();
+	int peer = atoi(source);
+	NeighborDiscPacket *rcvPacket;
+	DataPacket *dataPacket;
+
+	switch (msgType) {
+		case NEIGHBOR_DISC_PACKET:
+			trace() << "Discovered a neighbor! " << peer;
+			rcvPacket = check_and_cast<NeighborDiscPacket*> (genericPacket);
+			//Save neighbors schedule if it is being discovered for the first time.
+			if(neighborSchedules.count(peer) == 0) {
+				neighborSchedules[peer] = rcvPacket->getNodeSchedule();
+			}
+
+			//Respond with schedule if neighbor requested for it.
+			if(rcvPacket->getRequest()) {
+				Schedule mySchedule; //= new Schedule();
+				mySchedule.primePair[0] = primePair[0];
+				mySchedule.primePair[1] = primePair[1];
+				mySchedule.currentSlotNo = counter;
+				mySchedule.currentTime = getClock();
+
+				trace() << "Respond with schedule.";
+				toNetworkLayer(createNeighborDiscPacket(NEIGHBOR_DISC_PACKET, mySchedule, false, packetsSent++), source);
+			}
+			break;
+
+		case DATA_PACKET:
+			dataPacket = check_and_cast<DataPacket*> (genericPacket);
+			break;
+		default:
+			break;
+	}
 }
 
 void Disco::finishSpecific() {
-
+	trace() << "Total neighbors = " << neighborSchedules.size();
+	for(map<int, Schedule>::iterator it = neighborSchedules.begin(); it != neighborSchedules.end(); it++) {
+		trace() << "Neighbor : " << it->first;
+		trace() << "Schedule ";
+		Schedule schedule = it->second;
+		trace() << "Discovered at slot = " << schedule.currentSlotNo;
+		trace() << "Prime1 " << schedule.primePair[0] << " Prime2 " << schedule.primePair[1];
+		trace() << "Discovered at time = " << schedule.currentTime;
+		trace() << "";
+	}
 }
 
 void Disco::handleSensorReading(SensorReadingMessage * reading) {
--- Disco.h
+++ Disco.h
@@ -2,17 +2,39 @@
 #define _DISCO_H_
 
 #include "VirtualApplication.h"
-#include "DiscoPacket_m.h"
+#include "DataPacket_m.h"
+#include "NeighborDiscPacket_m.h"
 
 using namespace std;
+
+int primePairs[4][2] = {
+		{23, 157},
+		{29, 67},
+		{31, 53},
+		{37, 43}
+};
+int totalPrimes = 4;
+
+enum TIMERS {
+	START_OF_SLOT = 1, GO_TO_SLEEP = 2
+};
+
+enum PACKET_TYPE {
+	NEIGHBOR_DISC_PACKET = 1, DATA_PACKET = 2
+};
 
 class Disco: public VirtualApplication {
 private:
 	int packetsSent;
-	simtime_t neighbourCheckInterval, gossipInterval;
+	simtime_t slotDuration, gossipInterval;
 	double si, wi;
 	cModule *node, *wchannel, *network;
+	int primePair[2];
+	long counter;
+	bool isAsleep;
+	map<int, Schedule> neighborSchedules;
 
+	double unifRandom();
 protected:
 	void startup();
 	void finishSpecific();
@@ -20,6 +42,7 @@ protected:
 	void handleSensorReading(SensorReadingMessage *);
 	void handleNeworkControlMessage(cMessage *);
 	void fromNetworkLayer(ApplicationPacket *, const char *, double, double);
-	DiscoPacket* createDiscoDataPacket(double data, ExchngInfo& extra,	unsigned int seqNum);
+	DataPacket* createDataPacket(PACKET_TYPE type, GossipData& extra, unsigned int seqNum);
+	NeighborDiscPacket* createNeighborDiscPacket(PACKET_TYPE type, Schedule& schedule, bool request, unsigned int seqNum);
 };
 #endif
